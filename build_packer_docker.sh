5d55d3fc1207773801d3a0bfbc5a500d90b7d99e
apolloclark <apolloclark@gmail.com>
2018-11-21 19:49:02 -0500

    added build script for all beats

---
+++ build_packer_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+# set your Docker hub username
+export DOCKER_USERNAME="apolloclark"
+
+# build auditbeat
+cd ./auditbeat
+./build_packer_docker.sh
+rspec ./spec/Dockerfile_spec.rb
+
+# build filebeat
+cd ../filebeat
+./build_packer_docker.sh
+rspec ./spec/Dockerfile_spec.rb
+
+# build metricbeat
+cd ../metricbeat
+./build_packer_docker.sh
+rspec ./spec/Dockerfile_spec.rb
+
+# build heartbeat
+cd ../heartbeat
+./build_packer_docker.sh
+rspec ./spec/Dockerfile_spec.rb
