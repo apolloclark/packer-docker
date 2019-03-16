e0d4af2e38fa29b91a9a3d9d0ef11cdfdc538b5d
apolloclark <apolloclark@gmail.com>
2019-03-16 19:13:30 -0400

    added packer submodules

--- build_test.sh
+++ build_test.sh
@@ -5,40 +5,74 @@ cd ./openjdk
 
 # build openjdk-jdk
 cd ./openjdk-jdk
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
-
-# build openjdk-jdk-headless
-cd ../openjdk-jdk-headless
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
-
-# build openjdk-jre
-cd ../openjdk-jre
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
-
-# build openjdk-jre-headless
-cd ../openjdk-jre-headless
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
-
-# build oraclejdk
-cd ../oraclejdk
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
+./build_packer_docker_all.sh
+cd ../
 
 
 
 # build tomcat
-cd ../tomcat
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
+cd ../packer-tomcat
+./build_packer_docker_all.sh
+
+# build elasticsearch
+cd ../elasticsearch
+./build_packer_docker_all.sh
+
+
+
+# build osquery
+cd ../packer-osquery
+./build_packer_docker_all.sh
+
+# build ruby
+cd ../packer-ruby
+./build_packer_docker_all.sh
+
+# build nodejs
+cd ../packer-nodejs
+./build_packer_docker_all.sh
+
+# build cloudsploit
+cd ../packer-cloudsploit
+./build_packer_docker_all.sh
+
+exit 0;
+
+
+
+# build kibana
+cd ../kibana
+./build_packer_docker_all.sh
+
+# build logstash
+cd ../logstash
+./build_packer_docker_all.sh
+
+# build zookeeper
+cd ../zookeeper
+./build_packer_docker_all.sh
 
 # build kafka
 cd ../kafka
-./build_packer_docker.sh
-rspec ./spec/Dockerfile_spec.rb
+./build_packer_docker_all.sh
+
+# build jenkins
+cd ../jenkins
+./build_packer_docker_all.sh
+
+# build jmeter
+cd ../jenkins
+./build_packer_docker_all.sh
+
+
+
+# build postgres
+cd ../postgres
+./build_packer_docker_all.sh
+
+# build redis
+cd ../kafka
+./build_packer_docker_all.sh
 
 
 
@@ -66,3 +100,4 @@ rspec ./spec/Dockerfile_spec.rb
 cd ../packetbeat
 ./build_packer_docker.sh
 rspec ./spec/Dockerfile_spec.rb
+
