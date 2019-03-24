6597fb73b7c53f0281dbcf3e33f08f5da169ed00
apolloclark <apolloclark@gmail.com>
2019-03-24 19:59:50 -0400

    updated Elastic to 6.6.2

--- openjdk/openjdk-jdk/scripts/install_basics.sh
+++ openjdk/openjdk-jdk/scripts/install_basics.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -eux
 
 if [ -x "$(command -v apt-get)" ]; then
-	apt-get update
-	apt-get upgrade -y
-    apt-get install -y aptitude python-minimal apt-transport-https ca-certificates-java git bzip2 unzip xz-utils
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update
+    apt-get upgrade -yq
+    apt-get install -yq aptitude python-minimal apt-transport-https ca-certificates-java git bzip2 unzip xz-utils
 fi
 
 if [ -x "$(command -v yum)" ]; then
-	yum update -y
-	yum install -y which nano curl wget git initscripts
+    yum update -y
+    yum install -y which nano curl wget git initscripts
 fi
--- zookeeper/spec/Dockerfile_spec.rb
+++ zookeeper/spec/Dockerfile_spec.rb
@@ -7,8 +7,9 @@ Docker.validate_version!
 
 describe "Dockerfile" do
   before(:all) do
-    image = Docker::Image.get("apolloclark/tomcat:latest")
-
+    image = Docker::Image.get(
+      ENV['DOCKER_USERNAME'] + "/" + ENV['PACKAGE'] + ":" + ENV['PACKAGE_VERSION']
+    )
     # https://github.com/mizzy/specinfra
     # https://docs.docker.com/engine/api/v1.24/#31-containers
     # https://github.com/swipely/docker-api
@@ -25,62 +26,10 @@ describe "Dockerfile" do
   def sys_user
     command("whoami").stdout.strip
   end
-  
-  def tomcat_version
-    command("cd /usr/local/tomcat/lib && java -cp catalina.jar org.apache.catalina.util.ServerInfo").stdout
-  end
 
 
 
   it "installs the right version of Ubuntu" do
     expect(os_version).to include("Ubuntu")
-  end
-
-  it "installs tomcat version" do
-    expect(tomcat_version).to include("9.0.14.0")
-  end
-
-  it "runs as tomcat user" do
-    expect(sys_user).to eql("tomcat")
-  end
-
-  describe user('tomcat') do
-    it { should exist }
-    it { should have_uid 1000 }
-    it { should belong_to_group "tomcat" }
-    it { should have_login_shell '/bin/bash' }
-  end
-
-  describe group('tomcat') do
-    it {should exist}
-    it { should have_gid 1000}
-  end
-  
-  describe file('/usr/local/tomcat') do
-    it { should exist }
-    it { should be_directory }
-    it { should be_owned_by 'tomcat' }
-    it { should be_mode 755 }
-  end
-
-  describe file('/usr/local/tomcat/bin') do
-    it { should exist }
-    it { should be_directory }
-    it { should be_owned_by 'tomcat' }
-    it { should be_mode 750 }
-  end
-  
-  describe file('/usr/local/tomcat/conf') do
-    it { should exist }
-    it { should be_directory }
-    it { should be_owned_by 'tomcat' }
-    it { should be_mode 750 }
-  end
-
-  describe file('/usr/local/tomcat/webapps') do
-    it { should exist }
-    it { should be_directory }
-    it { should be_owned_by 'tomcat' }
-    it { should be_mode 750 }
   end
 end
